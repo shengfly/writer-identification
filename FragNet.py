9018b16983c30113b17d7f4f2dcbc82fb1056159
Sheng He <heshengxgd@gmail.com>
2021-03-31 10:14:38 -0400

    Update FragNet.py

--- FragNet.py
+++ FragNet.py
@@ -3,173 +3,184 @@
 # @author: Sheng He
 # @Email: heshengxgd@gmail.com
 # Github: https://github.com/shengfly/writer-identification
+
+
+# Sheng He, Lambert Schomaker,  FragNet: Writer Identification Using Deep Fragment Networks,
+# IEEE Transactions on Information Forensics and Security ( Volume: 15), Pages: 3013-3022
+# @Arixv: https://arxiv.org/pdf/2003.07212.pdf
+
 #*************************
 
-'''
-@ usage: load the image for PyTorch
-'''
 
-import os
-import pickle
-import numpy as np
-import torch.utils.data as data
 import torch
-from torchvision.transforms import Compose, ToTensor
-from PIL import Image
-import torchvision.transforms.functional as TF
-from scipy import misc
-import random
+import torch.nn as nn
 
-class WriterDataLoader(data.Dataset):
-    def __init__(self,dataset,folder,scale_size=(64,128),is_training=True):
+class VGGnet(nn.Module):
+
+    def __init__(self, input_channel):
         super().__init__()
         
-        self.is_training = is_training
+        layers=[64,128,256,512]
         
-        self.scale_size = scale_size
-        self.broader = 10
-        #self.resize_crop = [i+self.broader for i in self.scale_size]
+        self.conv1 = self._conv(input_channel,layers[0])
+        self.maxp1 = nn.MaxPool2d(2,stride=2)
+        self.conv2 = self._conv(layers[0],layers[1])
+        self.maxp2 = nn.MaxPool2d(2,stride=2)
+        self.conv3 = self._conv(layers[1],layers[2])
+        self.maxp3 = nn.MaxPool2d(2,stride=2)
+        self.conv4 = self._conv(layers[2],layers[3])
+        self.maxp4 = nn.MaxPool2d(2,stride=2)
         
-        self.folder = folder
-        self.dataset = dataset
         
-        self.labelidx_name = self.dataset+'_WRITER_LABEL_INDEX_DotMove.pkl'
-        self.imglist = self._get_image_list(self.folder)
-        
-        self.writer_label = self._convert_identity2index(self.labelidx_name)
-        self.num_writer = len(self.writer_label)
-        
-        print(dataset,' get image file:',len(self.imglist))
-        print('get number of writer:',len(self.writer_label))
-        
-    # convert to idx for neural network
-    def _convert_identity2index(self,savename):
-        if os.path.exists(savename):
-            print('label index dictory existed, load for reuse')
-            with open(savename,'rb') as fp:
-                identity_idx = pickle.load(fp)
-        else:
-            identity_idx = {}
-            idlist = self._get_all_identity()
-            for idx,ids in enumerate(idlist):
-                identity_idx[ids] = idx
-    
-            with open(savename,'wb') as fp:
-                pickle.dump(identity_idx,fp)
-    
-        return identity_idx
+    def _conv(self,inplance,outplance,nlayers=2):
+        conv = []
+        for n in range(nlayers):
+            conv.append(nn.Conv2d(inplance,outplance,kernel_size=3,
+                          stride=1,padding=1,bias=False))
+            conv.append(nn.BatchNorm2d(outplance))
+            conv.append(nn.ReLU(inplace=True))
+            inplance = outplance
             
-    def _get_image_list(self,folder,ftype='pickle'):
-        flist = os.listdir(folder)
-        imglist = []
-        for img in flist:
-            if img.endswith(ftype):
-                imglist.append(img)
-        return imglist
+        conv = nn.Sequential(*conv)
+               
+        return conv
     
-    
-    def _get_all_identity(self):
-        writer_list = []
-        for img in self.imglist:
-            writerId = self._get_identity(img)
-            writer_list.append(writerId)
-        writer_list=list(set(writer_list))
-        return writer_list
-    
-    def _get_identity(self,fname):
-        if self.dataset == 'CERUG':
-            return fname.split('_')[0]
-        else: return fname.split('-')[0]
-    
-    def _resize(self,sample,scale_size):
-        image = TF.resize(sample,scale_size)
-        return image
-    
-    def transform(self,sample):
-        image = Image.fromarray(sample,mode='L')
+    def forward(self, x):
+        xlist=[x]
+        x = self.conv1(x)
+        xlist.append(x)
+        x = self.maxp1(x)
+        x = self.conv2(x)
+        xlist.append(x)
+        x = self.maxp2(x)
+        x = self.conv3(x)
+        xlist.append(x)
+        x = self.maxp3(x)
+        x = self.conv4(x)
+        xlist.append(x)
+        return xlist
+
+
         
-        if self.is_training:
-            image = self._resize(image,self.resize_crop)
-            top = np.random.randint(self.broader)
-            left = np.random.randint(self.broader)
-            image = TF.crop(image,top,left,self.scale_size[0],self.scale_size[1])
-            if np.random.randint(10) < 5:
-                image = TF.hflip(image)
-        else:
-            image = self._resize(image,self.scale_size)
+class FragNet(nn.Module):
+    def __init__(self,inplace,num_classes):
+        super().__init__()
+        
+        self.net = VGGnet(inplace)
+        
+        layers=[64,128,256,512,512]
+        
+        self.conv0 = self._conv(inplace,layers[0])
+        self.conv1 = self._conv(layers[0]*2,layers[1])
+        self.maxp1 = nn.MaxPool2d(2,stride=2)
+        self.conv2 = self._conv(layers[1]*2,layers[2])
+        self.maxp2 = nn.MaxPool2d(2,stride=2)
+        self.conv3 = self._conv(layers[2]*2,layers[3])
+        self.maxp3 = nn.MaxPool2d(2,stride=2)
+        self.conv4 = self._conv(layers[3]*2,layers[4])
+        self.maxp4 = nn.MaxPool2d(2,stride=2)
+        
+        self.avg = nn.AdaptiveAvgPool2d(1)
+        self.classifier = nn.Linear(512,num_classes)
+        
+    def _conv(self,inplance,outplance,nlayers=2):
+        conv = []
+        for n in range(nlayers):
+            conv.append(nn.Conv2d(inplance,outplance,kernel_size=3,
+                          stride=1,padding=1,bias=False))
+            conv.append(nn.BatchNorm2d(outplance))
+            conv.append(nn.ReLU(inplace=True))
+            inplance = outplance
             
-        image = Compose([ToTensor()])(image)
-        return image
+        conv = nn.Sequential(*conv)
+               
+        return conv
     
-    def _resize_ration(self,image):
+    def forward(self,x):
+        xlist = self.net(x)
         
-        h,w = image.shape[:2]
-        ratio_h = float(self.scale_size[0])/float(h)
-        ratio_w = float(self.scale_size[1])/float(w)
+        step = 16
         
-        if ratio_h < ratio_w:
-            ratio = ratio_h
-        else:
-            ratio = ratio_w
-    
-        nh = int(ratio * h)
-        nw = int(ratio * w)
+        #print(xlist[0].shape)
         
-        imre = misc.imresize(image,(nh,nw))
+        # input image
+        reslist = []
+        for n in range(0,65,step):
+            xpatch = xlist[0][:,:,:,n:n+64]
+            r = self.conv0(xpatch)
+            reslist.append(r)
         
-        imre = 255 - imre
-        ch,cw = imre.shape[:2]
+        # 0-layer
+        idx = 0
+        res1list = []
+        for n in range(0,65,step):
+            xpatch = xlist[1][:,:,:,n:n+64]
+            xpatch = torch.cat([xpatch,reslist[idx]],1)
+            idx += 1
+            r = self.conv1(xpatch)
+            r = self.maxp1(r)
+            res1list.append(r)
         
-        new_img = np.zeros(self.scale_size)
+        # 1-layer
+        idx = 0
+        res2list = []
+        step = 8
+        for n in range(0,33,step):
+            xpatch = xlist[2][:,:,:,n:n+32]
+            xpatch = torch.cat([xpatch,res1list[idx]],1)
+            idx += 1
+            r = self.conv2(xpatch)
+            r = self.maxp2(r)
+            res2list.append(r)
         
-        if self.is_training:
-            dy = int((self.scale_size[0]-ch))
-            dx = int((self.scale_size[1]-cw))
-            dy = random.randint(0,dy)
-            dx = random.randint(0,dx)
-        else:
-            dy = int((self.scale_size[0]-ch)/2.0)
-            dx = int((self.scale_size[1]-cw)/2.0)
-    
-        imre = imre.astype('float')
-    
-        new_img[dy:dy+ch,dx:dx+cw] = imre
+        # 2-layer
         
-        return new_img
-
-    def _loadPickle(self,fname):
-        with open(fname,'rb') as fp:
-            data = pickle.load(fp)
-        return data
-    
-    def __getitem__(self,index):
-                
-        imgfile = self.imglist[index]
-        writer = self.writer_label[self._get_identity(imgfile)]
-        
-        image = self._loadPickle(self.folder + imgfile)
-        #print('image:',image.shape)
-        image = self._resize_ration(image)
-        #image = self.transform(image)
-        
-        image = Compose([ToTensor()])(image)
-        #print(image.max())
-        #image = 1.0 - image
-        writer = torch.from_numpy(np.array(writer))
-
-        return image,writer
+        idx = 0
+        res3list = []
+        step = 4
+        for n in range(0,17,step):
+            xpatch = xlist[3][:,:,:,n:n+16]
+            xpatch = torch.cat([xpatch,res2list[idx]],1)
+            idx += 1
+            r = self.conv3(xpatch)
+            r = self.maxp3(r)
+            res3list.append(r)
             
-    def __len__(self):
-        return len(self.imglist)
+        # 3-layer
+        idx = 0
+        step = 2
+        logits_list = []
 
+        for n in range(0,9,step):
+            xpatch = xlist[4][:,:,:,n:n+8]
+            xpatch = torch.cat([xpatch,res3list[idx]],1)
+            idx += 1
+            r = self.conv4(xpatch)
+            r = self.maxp4(r)
+            r = torch.flatten(self.avg(r),1)
+
+            c = self.classifier(r)
+            
+            logits_list.append(c)
+            
+        
+        combined_logits = 0
+        for r in logits_list:
+            combined_logits += r
+            
+        
+        return logits_list,combined_logits
+        
 
 if __name__ == '__main__':
-    loader = WriterDataLoader('IAM','../residualReLU/IAM_pickle/test/',is_training=True)
-    import matplotlib.pyplot as plt
-    for n in range(0,len(loader)):
-        image,writer = loader.__getitem__(n)
-        image = image[0].cpu().numpy()
-        plt.imshow(image)
-        print(image.shape,writer)
-        break
+    x = torch.rand(4,1,64,128)
+    
+    mod = FragNet(1,105)
+    ylist,c = mod(x)
+    for y in ylist:
+        print(y.shape)
+    print(c.shape)
+  
+    
+
 
